a534f78a07d4a8507094cfa30baff86c21976d2d
CristianCapsuna <50058296+CristianCapsuna@users.noreply.github.com>
2021-10-28 15:49:04 +0100

    making sure negative numbers are not accepted by the check_int function

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -4,6 +4,8 @@ from os import system
 def check_if_int(user_input) -> int:
     try:
         my_int = int(user_input)
+        if my_int < 0:
+            return -1
         return my_int
     except ValueError:
         return -1
@@ -11,6 +13,8 @@ def check_if_int(user_input) -> int:
 def check_if_float(user_input) -> float:
     try:
         my_float = float(user_input)
+        if my_float < 0:
+            return -1
         return my_float
     except ValueError:
         return -1
